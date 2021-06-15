5018c8743d5e544437aa4d4d04847a2e2a1c4281
itsirajul <shirajulislamparvez@gmail.com>
2021-06-15 22:04:43 +0600

    fixing the bug! Successfully run the program on Terminal

--- src/07.Pattern_Question/05.Half_Pyramid_Using_Numbers.cpp
+++ src/07.Pattern_Question/05.Half_Pyramid_Using_Numbers.cpp
@@ -8,10 +8,11 @@ int main()
 
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=1;j++)
+        for(int j=1;j<=i;j++)
         {
             cout<<i<<" ";
         }
         cout<<endl;
     }
+    return 0;
 }
