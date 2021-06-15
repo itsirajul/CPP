65a15086a5cc9be83b790f56963376587f62b6bb
itsirajul <shirajulislamparvez@gmail.com>
2021-06-15 22:00:10 +0600

    Use Nested For Loop and taking numbers from users

--- src/07.Pattern_Question/05.Half_Pyramid_Using_Numbers.cpp
+++ src/07.Pattern_Question/05.Half_Pyramid_Using_Numbers.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=1;j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
 }
