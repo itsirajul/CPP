1025fc1fbdbaa267f8a98e708d5b48a1e4544e60
itsirajul <shirajulislamparvez@gmail.com>
2021-06-19 22:40:08 +0600

    Palindrome

--- src/08.Advance_Pattern/05.Palindromic_Pattern.cpp
+++ src/08.Advance_Pattern/05.Palindromic_Pattern.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+    for(int i=1;i<=1;i++)
+    {
+        int j;
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<"  ";
+        }
+        int k=i;
+        for(;j<=n;j++)
+        {
+            cout<<k--<<" ";
+        }
+        k=2;
+        for(;j<=n+i-1;j++)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
