ef5c149dd55c380708ebe4d52785b255b8d3d9e1
itsirajul <shirajulislamparvez@gmail.com>
2021-06-20 07:33:53 +0600

    first bug fixing

--- src/08.Advance_Pattern/06.Star_Pattern.cpp
+++ src/08.Advance_Pattern/06.Star_Pattern.cpp
@@ -11,18 +11,18 @@ int main()
             cout<<" ";
         }
         for(int j=1;j<=2*i-1;i++){
-            cout<<" ";
+            cout<<"*";
         }
         cout<<endl;
     }
-
     for(int i=n;i>=1;i--){
         for(int j=1;j<=n-i;j++){
             cout<<" ";
         }
         for(int j=1;j<=2*i-1;i++){
-            cout<<" ";
+            cout<<"*";
         }
         cout<<endl;
     }
+    return 0;
 }
