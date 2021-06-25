c0ea4d36e69ef298b49a946ace827a2b83a4c651
itsirajul <shirajulislamparvez@gmail.com>
2021-06-25 22:03:07 +0600

    Reverse

--- src/09.Number_System/02.Reverse_number.cpp
+++ src/09.Number_System/02.Reverse_number.cpp
@@ -10,7 +10,7 @@ int main()
     while(n>0)
     {
         int lastdigit=n%10;
-        reverse=reverse*10+lastdigit;
+        reverse= reverse*10 + lastdigit;
         n=n/10;
     }
     cout<<reverse;
