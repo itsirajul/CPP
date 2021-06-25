58c64d4afbf8f47793643fddd64e43af89d678c7
itsirajul <shirajulislamparvez@gmail.com>
2021-06-25 22:17:43 +0600

    ArmStrong Number-Almost Finished

--- src/09.Number_System/03.Armstrong_Number.cpp
+++ src/09.Number_System/03.Armstrong_Number.cpp
@@ -7,4 +7,21 @@ int main()
     cin>>n;
 
     int sum=0;
+    int originaln=n;
+    while(n>0)
+    {
+        int lastdigit=n%10;
+        sum+=pow(lastdigit,3);
+        n=n/10;
+    }
+    if(sum--originaln)
+    {
+        cout<<"Armstrong Number"<<endl;
+    }
+    else
+    {
+        cout<<"Not Armstrong"<<endl;
+    }
+
+    return 0;
 }
