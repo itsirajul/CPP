0128d16185784c70026670f8fad9c5fc3263d385
itsirajul <shirajulislamparvez@gmail.com>
2021-06-26 21:55:17 +0600

    Build Origirnal Function to Reduce Work

--- src/10.Function/01.Print_Prime_Number.cpp
+++ src/10.Function/01.Print_Prime_Number.cpp
@@ -2,8 +2,21 @@
 #include<math.h>
 using namespace std;
 
-int main()
+bool isPrime(int num)
 {
+    for(int i=2;i<=sqrt(num);i++)
+    {
+        if(num%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{  
+
     int a,b;
     cin>>a>>b;
 
