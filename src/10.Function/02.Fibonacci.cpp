542fd7915662fd0ebc5830945b1ab669911204ce
itsirajul <shirajulislamparvez@gmail.com>
2021-06-27 18:46:33 +0600

    Fibonacchi

---
+++ src/10.Function/02.Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void fib(int n)
+{
+    int t1=0;
+    int t2=1;
+    int nextTerm;
+
+    for(int i=1;i<=n;i++)
+    {
+        
+    }
+}
+int main()
+{
+
+}
