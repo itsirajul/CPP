e76e677d20f029b5e970700d1210cdd3fef2ac41
itsirajul <shirajulislamparvez@gmail.com>
2021-06-27 18:46:54 +0600

    Build Main Function

--- src/10.Function/02.Fibonacci.cpp
+++ src/10.Function/02.Fibonacci.cpp
@@ -9,10 +9,19 @@ void fib(int n)
 
     for(int i=1;i<=n;i++)
     {
-        
+        cout<<t1<<endl;
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
     }
+    return;
 }
 int main()
 {
+    int n;
+    cin>>n;
 
+    fib(n);
+
+    return 0;
 }
