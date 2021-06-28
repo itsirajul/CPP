cb99f8262a6afd199c960f87e8a81ff11774516a
itsirajul <shirajulislamparvez@gmail.com>
2021-06-28 23:41:36 +0600

    Build Sub Function

--- src/10.Function/05.Pascal_Triangle.cpp
+++ src/10.Function/05.Pascal_Triangle.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int fact(int n)
+{
+    int factorial=1;
+    for(int i=2;i<=n;i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+
 int main()
 {
     int n;
@@ -12,5 +22,6 @@ int main()
         {
              cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
         }
+        cout<<endl;
     }
 }
