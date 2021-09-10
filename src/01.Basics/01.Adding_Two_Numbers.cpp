ed8c6d474f4490a62343ab013ef0aaa2c0ffb9c5
itsirajul <shirajulislamparvez@gmail.com>
2021-09-11 00:06:58 +0600

    Adding Two Number

---
+++ src/01.Basics/01.Adding_Two_Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter Two Number";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Sum of Two Number is"<<c;
+    return 0;
+}
