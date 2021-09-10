29a4fa7178650eb102cf2f355dcd0a34ed90920d
itsirajul <shirajulislamparvez@gmail.com>
2021-09-11 00:19:36 +0600

    Reading Names

---
+++ src/01.Basics/02.Reading_Name.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    std::cin>>name;
+    std::cout<<"Hello Mr/Ms "<<name;
+
+    return 0;
+
+}
